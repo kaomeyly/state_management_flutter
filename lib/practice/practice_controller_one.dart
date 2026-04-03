f16401e8da6afa10c3f5bb4dedd26df267ec2410
Kao Meyly <kaomeyly@gmail.com>
2026-04-03 08:45:11 +0700

    update practice one of size

--- lib/practice/practice_controller_one.dart
+++ lib/practice/practice_controller_one.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 
-class PracticeControllerOne extends GetxController{
+class PracticeControllerOne extends GetxController {
   var selectedColor = "red".obs;
+  var selectedSize = "200".obs;
 }
--- lib/practice/practice_one.dart
+++ lib/practice/practice_one.dart
@@ -14,11 +14,14 @@ class _PracticeOneState extends State<PracticeOne> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(child: _buildbox()),
-          Expanded(child: _buildbtn()),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            Expanded(child: _buildbox()),
+            Expanded(child: _buildbtn()),
+          ],
+        ),
       ),
     );
   }
@@ -30,7 +33,13 @@ class _PracticeOneState extends State<PracticeOne> {
           : controller.selectedColor.value == "green"
           ? Colors.green
           : Colors.blue;
-      return Center(child: Container(width: 150, height: 150, color: color));
+      return Center(
+        child: Container(
+          width: double.parse(controller.selectedSize.value),
+          height: double.parse(controller.selectedSize.value),
+          color: color,
+        ),
+      );
     });
   }
 
@@ -39,6 +48,12 @@ class _PracticeOneState extends State<PracticeOne> {
       spacing: 10,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        Obx(
+          () => Text(
+            "Color : ${controller.selectedColor.value}",
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
         Row(
           spacing: 10,
           children: [
@@ -59,6 +74,57 @@ class _PracticeOneState extends State<PracticeOne> {
                 controller.selectedColor.value = "blue";
               },
               child: Container(width: 40, height: 40, color: Colors.blue),
+            ),
+          ],
+        ),
+        Text("Size : 200", style: TextStyle(fontSize: 20)),
+        Row(
+          spacing: 10,
+          children: [
+            OutlinedButton(
+              onPressed: () {
+                controller.selectedSize.value = "200";
+              },
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: BorderSide(color: Colors.brown, width: 2),
+              ),
+              child: Text(
+                "200",
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                controller.selectedSize.value = "150";
+              },
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: BorderSide(color: Colors.brown, width: 2),
+              ),
+              child: Text(
+                "150",
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                controller.selectedSize.value = "100";
+              },
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: BorderSide(color: Colors.brown, width: 2),
+              ),
+              child: Text(
+                "100",
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
             ),
           ],
         ),
